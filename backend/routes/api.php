ac9815b58ca68ebb166a7d11ed8f20d4141ef183
Gerardo0899 <GERARDO.JIMENEZVILLALOBOS@UCR.AC.CR>
2024-06-17 14:30:50 -0600

    Migraciones, modelos y conexion

---
+++ backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+//use App\Http\Controllers\DriverController;
+use App\Http\Controllers\LoginController;
+//use App\Http\Controllers\TripController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
+
+
